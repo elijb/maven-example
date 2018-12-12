32ab0a304c51304e957a495c4d5b050c0c42eed5
elijb <37274379+elijb@users.noreply.github.com>
2018-12-12 19:10:40 +0200

    Update Test01.java

--- src/test/java/Test01.java
+++ src/test/java/Test01.java
@@ -3,7 +3,12 @@ import static org.junit.Assert.assertEquals;
 
 public class Test01 {
 
-    @Test public void mytest() {
+    @Test public void mytest01() {
+        String str = "my string";
+	assertEquals("my string", str);
+    }
+    
+    @Test public void mytest02() {
         String str = "my string";
 	assertEquals("my string", str);
     }
